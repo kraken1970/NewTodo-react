d984e7ae7dcdc65f7caebdce87dd176ad81353b1
Serg Minin <everestdon@ukr.net>
2018-12-02 18:17:45 +0300

    init project and add bootstrap.css

---
+++ src/components/todo-list.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TodoListitem from "./todo-list-item";
+import "./todo-list.css";
+
+const TodoList = ({ todos }) => {
+  const elements = todos.map(item => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListitem {...itemProps} />
+      </li>
+    );
+  });
+
+  return <ul className="list-group todo-list">{elements}</ul>;
+};
+
+export default TodoList;
