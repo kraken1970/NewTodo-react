b634bf8d48697f8c280615036eefa9f860380400
Serg Minin <everestdon@ukr.net>
2018-12-14 13:40:19 +0300

    deploy in gh-pages

---
+++ build/precache-manifest.d0a64a18930ca4f1d28fbd776bcb940b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4cde690bdf2115bc852",
+    "url": "/deploy-todo/static/js/runtime~main.d4cde690.js"
+  },
+  {
+    "revision": "e5793d32bc54f41d9b50",
+    "url": "/deploy-todo/static/js/main.e5793d32.chunk.js"
+  },
+  {
+    "revision": "bc951937f3a44b7747ac",
+    "url": "/deploy-todo/static/js/1.bc951937.chunk.js"
+  },
+  {
+    "revision": "e5793d32bc54f41d9b50",
+    "url": "/deploy-todo/static/css/main.2c307019.chunk.css"
+  },
+  {
+    "revision": "f56c48823c35b3c5072d4c79aaccc06a",
+    "url": "/deploy-todo/index.html"
+  }
+];
